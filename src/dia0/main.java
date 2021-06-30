4b1fbe400b2e94bd66c6d642011d795da20cfe8f
carolgiordano <carolinaangiordano@gmail.com>
2021-06-30 00:34:09 -0300

    commit inicial.

---
+++ src/dia0/main.java
@@ -0,0 +1,9 @@
+package dia0;
+
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Olá talentos Tech Bank");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
