e947a82a2113c858c33bd592637b5de2ca3cb92c
carolgiordano <carolinaangiordano@gmail.com>
2021-07-21 22:18:59 -0300

    sql aula 01

---
+++ src/sql/ExemploSQLite.java
@@ -0,0 +1,27 @@
+package sql;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ExemploSQLite {
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/carol/Desktop/" +
+                    "Hime - Meus Arquivos/PROG/Tech Bank/SQLite/banco-aula-1/exercicio-sql.db");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
---
+++ src/sql/main.java
@@ -0,0 +1,24 @@
+package sql;
+
+import java.sql.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:/Users/carol/Desktop/" +
+                "Hime - Meus Arquivos/PROG/Tech Bank/SQLite/banco-aula-1/exercicio-sql.db")) {
+            try (Statement stmt = conn.createStatement()) {
+                String query = "select * from academia";
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()) {
+                    System.out.println("ID: " + rs.getInt("id"));
+                    System.out.println("NOME: " + rs.getString("nome"));
+                }
+                rs.close();
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
